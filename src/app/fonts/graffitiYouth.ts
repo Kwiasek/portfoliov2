a9dea155afc29e75bef28b0d0b6bdbae2a8282dc
kwiasek <kwiasekkwiasek@gmail.com>
2024-12-10 18:02:08 +0100

    Moved font to public folder

--- src/app/fonts/graffitiYouth.ts
+++ src/app/fonts/graffitiYouth.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const graffitiYouth = localFont({
-  src: "./fonts/GraffitiYouth-Regular.otf",
+  src: "../../../public/GraffitiYouth-Regular.otf",
   variable: "--font-graffiti-youth",
   weight: "400",
 });
