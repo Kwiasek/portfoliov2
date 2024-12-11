c970edce3e04bb7083caf4f939b3c91132c04056
kwiasek <kwiasekkwiasek@gmail.com>
2024-12-11 20:08:11 +0100

    Outline for logo

--- src/components/hero/Logo.tsx
+++ src/components/hero/Logo.tsx
@@ -3,7 +3,8 @@ import { graffitiYouth } from "@/app/fonts/graffitiYouth";
 export const Logo = ({ className = "" }: { className: string }) => {
   return (
     <div
-      className={`${graffitiYouth.className} ${className} text-primary drop-shadow-md select-none`}
+      className={`${graffitiYouth.className} ${className} text-primary drop-shadow-md select-none tracking-wider`}
+      style={{ WebkitTextStrokeColor: "#0d243b", WebkitTextStrokeWidth: 1 }}
     >
       Kwiasek
     </div>
