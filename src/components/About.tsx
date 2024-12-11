b539ce07f014bd71b95f6ca5e3add7b8027a3c64
kwiasek <kwiasekkwiasek@gmail.com>
2024-12-11 20:08:25 +0100

    Change of about me content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,11 +7,29 @@ export const About = () => {
     <section id="about">
       <SectionHeader>About</SectionHeader>
       <div className="flex gap-5 flex-col-reverse md:flex-row md:justify-between">
-        <p className="text-lg tracking-wide max-w-[80ch]">
-          Hi, my name is Adam Kwiasowski, I am currently Computer Science
-          student at Lodz University of Technology, PL. I’m learning Web
-          Development since high school, and I find it as an entertaining way to
-          challenge myself.
+        <p className="text-lg tracking-wider max-w-[80ch]">
+          Hi, my name is Adam Kwiasowski, and I&apos;m a Computer Science
+          student at Lodz University of Technology, PL. My journey into web
+          development started during my time at technical high school, where I
+          began exploring JavaScript. Later, I expanded my skills by diving into
+          React and SCSS through online courses. Over time, I grew familiar with
+          tools like Vite, Next.js, and TailwindCSS—technologies that now form
+          the backbone of my projects.
+          <br />
+          <br />
+          I&apos;ve nearly completed the comprehensive course{" "}
+          <em>The Odin Project</em>, which not only sharpened my frontend skills
+          but also introduced me to backend development. Some of my favorite
+          projects from the course are showcased in my portfolio. Among the
+          technologies I&apos;ve worked with, Next.js stands out as my favorite
+          for its versatility and growing ecosystem, making it a tool I believe
+          in for the future.
+          <br />
+          <br />
+          Currently, my focus is on university studies, where I&apos;m tackling
+          subjects like C, C++, computer graphics, and theoretical aspects of
+          computer science. Web development remains a passionate side pursuit,
+          allowing me to stay creative and explore new technologies.
         </p>
         <Image
           src={hackerMind}
